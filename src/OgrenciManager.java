7a36d3e3bf685afb7b834a6e4d5ff621e1c57e55
ZGK <zehragkrl@gmail.com>
2024-02-12 21:31:29 +0300

    Initial commit

---
+++ src/OgrenciManager.java
@@ -0,0 +1,193 @@
+
+
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
+public class OgrenciManager extends Veritabani {
+
+    static Scanner scan = new Scanner(System.in);
+
+    public static void ogrenciMenu() throws InterruptedException {
+
+        boolean exit = false;
+
+
+        do {
+            System.out.println(
+                    "\n============= TECHNO STUDY BOOTCAMP =============\n" +
+                            "================== OGRENCI MENU =================\n" +
+                            "\t   1- Ogrenci Listesi Yazdir\n" +
+                            "\t   2- Soyisimden Ogrenci Bulma\n" +
+                            "\t   3- Sinif ve Sube Ile Ogrenci Bulma\n" +
+                            "\t   4- Bilgilerini Girerek Ogrenci Ekleme\n" +
+                            "\t   5- Kimlik No Ile Kayit Silme \t\n" +
+                            "\t   A- ANAMENU\n" +
+                            "\t   Q- CIKIS");
+
+            String secim = scan.nextLine();
+
+            switch (secim) {
+
+
+                case "1":
+                    ogrenciListeYazdir();
+                    break;
+
+                case "2":
+                    soyisimdenOgrenciBulma();
+                    break;
+
+                case "3":
+                    sinifVeSubeIleOgrenciBulma();
+                    break;
+
+                case "4":
+                    ogrenciEkle();
+                    break;
+
+                case "5":
+                    tcNoIleOgrenciSilme();
+                    break;
+
+                case "a":
+                    Helper.anaMenu();
+                    break;
+
+                case "q":
+                    Helper.projeDurdur();
+                    return;
+
+                default:
+                    System.out.println("Lutfen gecerli tercih yapiniz:");
+                    break;
+            }
+
+
+        } while (!exit);
+
+
+    }
+
+    private static void tcNoIleOgrenciSilme() throws InterruptedException {
+        System.out.println("Silinecek ogrenci kimlik no giriniz");
+        String silinecekOgrenci = scan.nextLine();
+
+        String silinecekValue = ogrenciMap.get(silinecekOgrenci);
+        String sonucValue = ogrenciMap.remove(silinecekOgrenci);
+
+        System.out.print(silinecekOgrenci + "Siliniyor...");
+        for (int i = 0; i < 20; i++) {
+            Thread.sleep(100);
+            System.out.print(">");
+        }
+        //  Programın çalışmaya devam etmesi için gerekli
+        try {
+            boolean sonuc = sonucValue.equals(silinecekValue);
+        } catch (Exception e) {
+            System.out.println("Istediginiz TC numarasi ile ogrenci bulunamadi");
+        }
+
+    }
+
+
+    private static void ogrenciEkle() {
+
+        System.out.print("Öğrencinin TcNo'sunu giriniz :");
+        String ogrTcno = scan.nextLine();
+
+        System.out.print("Öğrencinin ismini giriniz : ");
+        String ogrIsim = scan.nextLine();
+
+        System.out.print("Öğrencinin soyadını giriniz :");
+        String ogrsoyAd = scan.nextLine();
+
+        System.out.print("Öğrencinin doğum yılını giriniz :");
+        String ogrDogumyili = scan.nextLine();
+
+        System.out.print("Öğrencinin okul numarasını giriniz :");
+        String ogrOkulNo = scan.nextLine();
+
+        System.out.print("Öğrencinin sınıfını giriniz :");
+        String ogrSinifi = scan.nextLine();
+
+        System.out.print("Öğrencinin şubesini giriniz :");
+        String ogrSube = scan.nextLine();
+
+
+        ogrenciMap.put(ogrTcno, ogrIsim + ", " + ogrsoyAd + ", " + ogrDogumyili + ", " + ogrOkulNo + ", " + ogrSinifi + ", " + ogrSube);
+
+
+    }
+
+    private static void sinifVeSubeIleOgrenciBulma() throws InterruptedException {
+
+        System.out.print("Öğrenci sınıfını giriniz : ");
+        String sinif = scan.nextLine();
+
+        System.out.print("Öğrenci şubesini giriniz :");
+        String sube = scan.nextLine();
+
+        System.out.print(sinif + " " + sube + " Öğrenciler Listeleniyor...");
+        for (int i = 0; i < 20; i++) {
+            Thread.sleep(100);
+            System.out.print(">");
+        }
+
+        Set<Map.Entry<String, String>> ogrenciEntrySet1 = ogrenciMap.entrySet();
+        System.out.println(
+                "\n============= TECHNO STUDY BOOTCAMP =============\n" +
+                        "============SOYADI ILE ÖĞRENCİ ARAMA ============\n" +
+                        "TcNo : Isim , Soyisim , D.Yili , Okul No, Sınıf, Şube");
+
+        for (Map.Entry<String, String> each1 : ogrenciEntrySet1) {
+            String eachkey1 = each1.getKey();
+            String eachValue1 = each1.getValue();
+
+            String[] eachValuar = eachValue1.split(", ");
+            if (sinif.equalsIgnoreCase(eachValuar[4]) && sube.equalsIgnoreCase(eachValuar[5])){
+                System.out.println("\n" + eachkey1 + " : " + eachValue1 + " | "); }
+        }
+
+    }
+
+    private static void soyisimdenOgrenciBulma() throws InterruptedException {
+
+        System.out.println("Aradiginiz Öğrencinin Soyadını Giriniz = ");
+        String istenenSoyad = scan.nextLine();
+
+        System.out.print(istenenSoyad + " Öğrenciler Listeleniyor...");
+        for (int i = 0; i < 20; i++) {
+            Thread.sleep(100);
+            System.out.print(">");
+        }
+
+        Set<Map.Entry<String, String>> ogrenciEntrySet = ogrenciMap.entrySet();
+        System.out.println(
+                "\n============= TECHNO STUDY BOOTCAMP =============\n" +
+                        "============SOYADI ILE ÖĞRENCİ ARAMA ============\n" +
+                        "TcNo : Isim , Soyisim , D.Yili , Okul No, Sınıf, Şube");
+
+        // Daha düzgün bi görünüm için printf veya String.format kullanılabilir... İsteğe bağlı
+        for (Map.Entry<String, String> each : ogrenciEntrySet) {
+            String eachKey = each.getKey();
+            String eachValue = each.getValue();
+
+            String[] eachValuarr = eachValue.split(", ");
+            if (istenenSoyad.equalsIgnoreCase(eachValuarr[1])) {
+                System.out.println("\n" + eachKey + " : " + eachValue + " | ");
+            }
+        }
+    }
+
+
+    private static void ogrenciListeYazdir() throws InterruptedException {
+
+        System.out.println("***** ÖĞRENCİLER LİSTESİ *****");
+
+        for (Map.Entry<String, String> ogrenciler : ogrenciMap.entrySet())
+            System.out.println(ogrenciler.getKey() + " " + ogrenciler.getValue());
+
+
+    }
+}
---
+++ src/OgretmenManager.java
@@ -0,0 +1,194 @@
+
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
+public class OgretmenManager extends Veritabani {
+
+    static Scanner scan = new Scanner(System.in);
+
+    public static void ogretmenMenu() throws InterruptedException {
+
+        boolean exit = false;
+
+
+        do {
+
+
+            System.out.println(
+                    "\n============= TECHNO STUDY BOOTCAMP =============\n" +
+                            "================= OGRETMEN MENU =================\n" +
+                            "\n" +
+                            "\t   1- Ogretmenler Listesi Yazdir\t\n" +
+                            "\t   2- Soyisimden Ogretmen Bulma\n" +
+                            "\t   3- Branstan Ogretmen Bulma\n" +
+                            "\t   4- Bilgilerini Girerek Ogretmen Ekleme\n" +
+                            "\t   5- Kimlik No Ile Kayit Silme \t\n" +
+                            "\t   A- ANAMENU\n" +
+                            "\t   Q- CIKIS\n");
+
+
+            String secim = scan.nextLine();
+
+            switch (secim) {
+
+
+                case "1":
+                    ogretmenListesiYazdir();
+                    break;
+
+                case "2":
+                    soyisimdenOgretmenBulma();
+                    break;
+
+                case "3":
+                    branstanOgretmenBulma();
+                    break;
+
+                case "4":
+                    ogretmenEkleme();
+                    break;
+
+                case "5":
+                    tcNoIleOgretmenSil();
+                    break;
+
+                case "a":
+                    Helper.anaMenu();
+                    break;
+
+                case "q":
+                    Helper.projeDurdur();
+                    return;
+                default:
+                    System.out.println("Lutfen gecerli tercih yapiniz:");
+                    break;
+
+            }
+
+
+        } while (!exit);
+
+    }
+
+
+    public static void tcNoIleOgretmenSil() throws InterruptedException {
+        System.out.println("Silinecek ogretmen kimlik no giriniz");
+        String silinecekOgretmen = scan.nextLine();
+
+        String silinecekValue = ogretmenlerMap.get(silinecekOgretmen);
+        String sonucValue = ogretmenlerMap.remove(silinecekOgretmen);
+
+        System.out.print(silinecekOgretmen + " Siliniyor...");
+        for (int i = 0; i < 20; i++) {
+            Thread.sleep(100);
+            System.out.print(">");
+        }
+
+        //////////////////////////////////////////////////////////////////////////////
+        // BU BLOĞU DEĞİŞTİRMEYİN, DİKKATE ALMAYIN...                               //
+        // SİLİNECEK KİMLİK NO YOKSA VEYA BAŞKA BEKLENMEDİK                         //
+        // ŞEYLER OLURSA KODUN DEVAMINI SAĞLAYACAK...
+        try {                                                                       //
+            boolean sonuc = sonucValue.equals(silinecekValue);                      //
+        } catch (Exception e) {                                                     //
+            System.out.println("Istediginiz Tc numarasi ile ogretmen bulunamadi");  //
+        }                                                                           //
+        //////////////////////////////////////////////////////////////////////////////
+    }
+
+
+    public static void ogretmenEkleme() {
+        System.out.print("Öğretmenin TcNo'sunu giriniz :");
+        String ogtTcno = scan.nextLine();
+
+        System.out.print("Öğretmenin ismini giriniz : ");
+        String ogtIsim = scan.nextLine();
+
+        System.out.print("Öğretmenin soyadını giriniz :");
+        String soyAd = scan.nextLine();
+
+        System.out.print("Öğretmenin doğum yılını giriniz :");
+        String ogrDogumyili = scan.nextLine();
+
+        System.out.print("Öğretmenin branşını giriniz :");
+        String ogtBrans = scan.nextLine();
+
+        ogretmenlerMap.put(ogtTcno, ogtIsim+", " + soyAd+", " + ogrDogumyili+", " + ogtBrans);
+
+    }
+
+
+    public static void branstanOgretmenBulma() throws InterruptedException {
+        System.out.println("Aradiginiz Ogretmenin Bransini Giriniz:");
+        String istenenBrans = scan.nextLine();
+
+        System.out.print(istenenBrans + " Ogretmenleri Listeleniyor...");
+        for (int i = 0; i < 20; i++) {
+            Thread.sleep(100);
+            System.out.print(">");
+        }
+
+        Set<Map.Entry<String, String>> ogretmenEntrySet = ogretmenlerMap.entrySet();
+        System.out.println(
+                "\n============= TECHNO STUDY BOOTCAMP =============\n" +
+                        "============BRANS ILE OGRETMEN ARAMA ============\n" +
+                        "TcNo : Isim , Soyisim , D.Yili , Brans");
+
+        // Daha düzgün bi görünüm için printf veya String.format kullanılabilir... İsteğe bağlı
+        for (Map.Entry<String, String> each : ogretmenEntrySet) {
+            String eachKey = each.getKey();
+            String eachValue = each.getValue();
+
+            String[] eachValuarr = eachValue.split(", ");
+            if (istenenBrans.equalsIgnoreCase(eachValuarr[3])) {
+                System.out.println(eachKey + " : " + eachValue + " | \n");
+            }
+        }
+    }
+
+    public static void soyisimdenOgretmenBulma() throws InterruptedException {
+
+        System.out.println("Aradiginiz Öğretmen'nin Soyadını Giriniz = ");
+        String istenenSoyad = scan.nextLine();
+
+        System.out.print(istenenSoyad + " Öğretmenler Listeleniyor...");
+        for (int i = 0; i < 20; i++) {
+            Thread.sleep(100);
+            System.out.print(">");
+        }
+
+        Set<Map.Entry<String, String>> ogretmenEntrySet = ogretmenlerMap.entrySet();
+        System.out.println(
+                "\n============= TECHNO STUDY BOOTCAMP =============\n" +
+                        "============SOYADI ILE ÖĞRETMEN ARAMA ============\n" +
+                        "TcNo : Isim , Soyisim , D.Yili , Branş");
+
+        // Daha düzgün bi görünüm için printf veya String.format kullanılabilir... İsteğe bağlı
+        for (Map.Entry<String, String> each : ogretmenEntrySet) {
+            String eachKey = each.getKey();
+            String eachValue = each.getValue();
+
+            String[] eachValuarr = eachValue.split(", ");
+            if (istenenSoyad.equalsIgnoreCase(eachValuarr[1])) {
+                System.out.println("\n" + eachKey + " : " + eachValue + " | ");
+            }
+        }
+
+    }
+
+
+
+    public static void ogretmenListesiYazdir() throws InterruptedException {
+
+        System.out.println("***** ÖĞRETMEN LİSTESİ ***** ");
+
+        for (Map.Entry<String, String> ogretmenlerListesi : ogretmenlerMap.entrySet())
+            System.out.println(ogretmenlerListesi.getKey() + " " + ogretmenlerListesi.getValue());
+
+
+    }
+}
+
+
+
